57d518d61be39dc3891d66e4888428f7af0aed16
Alexander Dadykin <DadykinAV@yandex.ru>
2016-05-25 11:46:03 +0300

    Add some comments to browserify entry point

--- vue-with-laravel/resources/assets/js/main.js
+++ vue-with-laravel/resources/assets/js/main.js
@@ -1,9 +1,12 @@
+// Include vue.js
 var Vue = require('vue');
 
+// import template and alert's logic
 import Alert from './components/Alert.vue';
 
 new Vue({
     el:'#app',
+    // components in use
     components: { Alert: Alert },
     ready() {
         alert("Ready to go");
