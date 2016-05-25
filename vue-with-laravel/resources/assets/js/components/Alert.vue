2e0721ab8b9502307db2e07599ae0c3aabcfe582
Alexander Dadykin <DadykinAV@yandex.ru>
2016-05-25 11:44:06 +0300

    Workflow of Vue.js with Laravel framework
    Use of browserify with Jeffrey's package laravel-elixir-vueify
    to compile templates and alert's logic in one Alert.vue file

---
+++ vue-with-laravel/resources/assets/js/components/Alert.vue
@@ -0,0 +1,21 @@
+<template id="alert-template">
+            <div :class="['alert', 'alert-'+type, 'alert-dismissible']" role="alert" v-show="visible">
+                <button v-on:click='visible = false' type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <slot></slot>
+            </div>
+</template>
+
+<script>
+export default {
+        props: ['type'],
+        data(){
+            return {
+                'visible': true
+            }
+        }
+};
+
+
+</script>
---
+++ vue-with-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body id="app">
+        <div class="container">
+            <alert type="success"><strong>Success!</strong> Task is done.</alert>
+
+            <alert type="danger"><strong>Error!</strong> Task wasn't completed.</alert>
+        </div>
+        <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+    </body>
+</html>
